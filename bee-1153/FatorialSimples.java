b297b87f2719151331af7548c7ff911302d60eb9
Lucas Pettinelli <pettinellilucas@gmail.com>
2024-12-15 21:18:54 -0300

    resolucao 1153

---
+++ bee-1153/FatorialSimples.java
@@ -0,0 +1,38 @@
+/*
+Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+
+Entrada
+A entrada contém um valor inteiro N (0 < N < 13).
+
+Saída
+A saída contém um valor inteiro, correspondente ao fatorial de N.
+
+Exemplo de Entrada	Exemplo de Saída
+4
+
+24
+*/
+
+import java.io.IOException;
+import java.util.Scanner; 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int fatorial = 1;
+        for(int i=1; i <= N; i++){
+            fatorial *=i;
+        }
+        System.out.println(fatorial);
+        scan.close();
+    }
+ 
+}
