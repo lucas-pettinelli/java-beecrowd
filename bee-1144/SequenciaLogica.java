fbcc826f0438bdac4839aeac49279e9cbcd6b859
Lucas Pettinelli <pettinellilucas@gmail.com>
2024-12-16 21:27:21 -0300

    resolucao bee 1144

---
+++ bee-1144/SequenciaLogica.java
@@ -0,0 +1,47 @@
+/*
+Escreva um programa que leia um valor inteiro N. N * 2 linhas de saída serão apresentadas na execução do programa, seguindo a lógica do exemplo abaixo. Para valores com mais de 6 dígitos, todos os dígitos devem ser apresentados.
+
+Entrada
+O arquivo de entrada contém um número inteiro positivo N (1 < N < 1000).
+
+Saída
+Imprima a saída conforme o exemplo fornecido.
+
+Exemplo de Entrada	Exemplo de Saída
+5
+
+1 1 1
+1 2 2
+2 4 8
+2 5 9
+3 9 27
+3 10 28
+4 16 64
+4 17 65
+5 25 125
+5 26 126
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        
+        for(int i=1; i<=N; i++){
+            System.out.println(i + " " + (i*i) + " " + (i*i*i));
+            System.out.println(i + " " + ((i*i)+1) + " " + ((i*i*i)+1));
+        }
+        scan.close();
+    }
+ 
+}
