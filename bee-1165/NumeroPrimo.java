54d7520cb47824b89d87882e7a59ea15ab365404
Lucas Pettinelli <pettinellilucas@gmail.com>
2024-12-21 22:54:15 -0300

    resolucao bee 1165

---
+++ bee-1165/NumeroPrimo.java
@@ -0,0 +1,56 @@
+/*
+Na matemática, um Número Primo é aquele que pode ser dividido somente por 1 (um) e por ele mesmo. Por exemplo, o número 7 é primo, pois pode ser dividido apenas pelo número 1 e pelo número 7.
+
+Entrada
+A entrada contém vários casos de teste. A primeira linha da entrada contém um inteiro N (1 ≤ N ≤ 100), indicando o número de casos de teste da entrada. Cada uma das N linhas seguintes contém um valor inteiro X (1 < X ≤ 107), que pode ser ou não, um número primo.
+
+Saída
+Para cada caso de teste de entrada, imprima a mensagem “X eh primo” ou “X nao eh primo”, de acordo com a especificação fornecida.
+
+Exemplo de Entrada	Exemplo de Saída
+3
+8
+51
+7
+
+8 nao eh primo
+51 nao eh primo
+7 eh primo
+*/
+
+import java.io.IOException;
+import java.util.Scanner; 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int X = scan.nextInt();
+            if (isPrime(X)) {
+                System.out.println(X + " eh primo");
+            } else {
+                System.out.println(X + " nao eh primo");
+            }
+        }
+
+        scan.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
